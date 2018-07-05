b799109215f929d0e652bf730c38071ded67b76d
Dolgubon <canopus100@gmail.com>
2018-07-04 21:50:00 -0400

    Removed the feedback.lua and feedback.xml files. Ported over to the LibFeedback format.

--- SetCrafter.lua
+++ SetCrafter.lua
@@ -107,7 +107,9 @@ function DolgubonSetCrafter:Initialize()
 	--if pcall(DolgubonSetCrafter.initializeFunctions.setupUI) then else d("Dolgubon's Lazy Set Crafter: UI not loaded") end
 	DolgubonSetCrafter.initializeFunctions.setupUI()
 	
-	DolgubonSetCrafter.initializeFeedbackWindow()
+	--DolgubonSetCrafter.initializeFeedbackWindow()
+	LibStub:GetLibrary("LibFeedback")
+	LibFeedback:initializeFeedbackWindow(DolgubonSetCrafter, "Dolgubon's Lazy Set Crafter",DolgubonSetCrafterWindow, "@Dolgubon", {TOPLEFT , owningWindow , TOPLEFT , 10, 10}, {0,5000,50000, "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7CZ3LW6E66NAU"})
 end
 
 local function closeWindow (optionalOverride)
